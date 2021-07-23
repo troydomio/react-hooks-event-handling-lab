9826262037398078fc373f9f4063d761702bb14a
Troy <troyakins99@yahoo.com>
2021-07-23 15:35:02 -0500

    commit tree d513125a9c642b83819da4a553fdbecce0982151

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+
+const EyesOnMe = () => {
+    return (
+        <>
+            <button 
+            onFocus= {()=>console.log('Good!')} 
+            onBlur={()=>console.log('Hey! Eyes on me!')}>Eyes on me 
+            </button>
+        </>
+    );
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+
+const Keypad = () => {
+    return(
+        <>
+            <input type="password" onChange={(()=> console.log("Entering password..."))}/>
+        </>
+    );
+}
+
+export default Keypad
